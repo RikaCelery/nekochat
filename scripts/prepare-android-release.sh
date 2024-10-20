ab7a1d1b592d65ec95d4e836d2723796d6d2e0e8
RikaCelery <qincai233@gmail.com>
2024-10-20 11:02:18 +0800

    fix keypath error

--- scripts/prepare-android-release.sh
+++ scripts/prepare-android-release.sh
@@ -4,7 +4,7 @@ echo $FDROID_KEY | base64 --decode --ignore-garbage > key.jks
 echo "storePassword=${FDROID_KEY_PASS}" >> key.properties
 echo "keyPassword=${FDROID_KEY_PASS}" >> key.properties
 echo "keyAlias=key" >> key.properties
-echo "storeFile=./key.jks" >> key.properties
+echo "storeFile=../key.jks" >> key.properties
 echo $PLAYSTORE_DEPLOY_KEY >> keys.json
 ls | grep key
 bundle install
